9f16d9841ff75114360ea730d9e3e232ce76e910
mikegsCoder <mikegsid@gmail.com>
2022-05-10 17:03:08 +0300

    Refactor skills view using templating

--- src/views/skills.js
+++ src/views/skills.js
@@ -1,44 +1,56 @@
 import { html } from '../lib.js';
 
-const skillsTemplate = () => html`
+const skillsTemplate = (devicons, skills) => html`
 <section class="resume-section" id="skills">
     <div class="resume-section-content">
         <h2 class="mb-5">Skills</h2>
         <div class="subheading mb-3">Programming Languages & Tools</div>
         <ul class="list-inline dev-icons">
-            <li class="list-inline-item"><i class="devicon-csharp-plain-wordmark colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-dot-net-plain-wordmark colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-dotnetcore-plain colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-microsoftsqlserver-plain-wordmark colored"></i></li>
-            <!-- <li class="list-inline-item"><i class="devicon-html5-plain-wordmark colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-css3-plain-wordmark colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-bootstrap-plain-wordmark colored"></i></li> -->
-            <li class="list-inline-item"><i class="devicon-javascript-plain colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-mocha-plain colored"></i></li>
-            <!-- <li class="list-inline-item"><i class="devicon-typescript-plain colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-angularjs-plain colored"></i></li>
-            <li class="list-inline-item"><i class="devicon-react-original-wordmark colored"></i></li> -->
+
+            ${devicons.map(deviconCard)}
+
         </ul>
 
         <div class="subheading mb-3 mt-4">Personal Skills</div>
         <ul class="fa-ul mb-0">
-            <li>
-                <i class="fa-li fa fa-arrow-right"></i> Algorithmic Thinking
-            </li>
-            <li>
-                <i class="fa-li fa fa-arrow-right"></i> Problem Solving
-            </li>
-            <li>
-                <i class="fa-li fa fa-arrow-right"></i> Communication
-            </li>
-            <li>
-                <i class="fa-li fa fa-arrow-right"></i> Teamwork
-            </li>
+
+            ${skills.map(skillCard)}
+
         </ul>
     </div>
 </section>
 <hr class="m-0" />`;
 
+const deviconCard = (devicon) => html`
+<li class="list-inline-item"><i class="${devicon}"></i></li>`;
+
+const skillCard = (skill) => html`
+<li>
+    <i class="fa-li fa fa-arrow-right"></i> ${skill}
+</li>`;
+
 export async function skillsPage(ctx) {
-    ctx.render(skillsTemplate());
+    ctx.render(skillsTemplate(devicons, skills));
 };
+
+const devicons = [
+    'devicon-csharp-plain-wordmark colored',
+    'devicon-dot-net-plain-wordmark colored',
+    'devicon-dotnetcore-plain colored',
+    'devicon-microsoftsqlserver-plain-wordmark colored',
+    // 'devicon-html5-plain-wordmark colored',
+    // 'devicon-css3-plain-wordmark colored',
+    // 'devicon-bootstrap-plain-wordmark colored',
+    'devicon-javascript-plain colored',
+    'devicon-mocha-plain colored',
+    // 'devicon-typescript-plain colored',
+    // 'devicon-angularjs-plain colored',
+    // 'devicon-react-original-wordmark colored'
+];
+
+const skills = [
+    'Algorithmic Thinking',
+    'Problem Solving',
+    'Communication',
+    'Teamwork'
+];
