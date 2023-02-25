31e8613acbcf55a855bafedb3401e5b111df3ea5
mikegsCoder <mikegsid@gmail.com>
2023-02-25 16:09:17 +0200

    Changed libraries source

--- exams/petCare/src/lib.js
+++ exams/petCare/src/lib.js
@@ -1,5 +1,5 @@
-import { html, render, nothing } from 'https://github.com/mikegsCoder.github.io/src/lib/lit-html/lit-html.js';
-import page from 'https://github.com/mikegsCoder.github.io/src/lib/page/page.mjs';
+import { html, render, nothing } from 'https://mikegsCoder.github.io/src/lib/lit-html/lit-html.js';
+import page from 'https://mikegsCoder.github.io/src/lib/page/page.mjs';
 
 export {
     html,
